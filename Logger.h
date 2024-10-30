f83d780cecd017962c5c7bc468c97b6c3f631df1
MohamedElashri <mail@elashri.com>
2024-10-30 17:07:27 -0400

    Add functions comment
    Authored-by: Mohamed Elashri <mail@elashri.com>

--- Logger.h
+++ Logger.h
@@ -58,11 +58,37 @@ struct LogMessage {
  */
 class Output {
 public:
+    /**
+     * @brief Constructor for Output.
+     *
+     * @param os The output stream where the log messages are written.
+     * @param level The log level for this output.
+     * @param format The timestamp format.
+     * @param outputFormat The log message output format.
+     * @param useANSIColors Whether to use ANSI escape sequences to color log messages.
+     */
     Output(std::shared_ptr<std::ostream> os, LogLevel level, const std::string& format = "%Y-%m-%d %H:%M:%S",
            const std::string& outputFormat = "plain", bool useANSIColors = true)
         : stream(std::move(os)), logLevel(level), timestampFormat(format),
           outputFormat(outputFormat), useANSIColors(useANSIColors) {}
 
+    /**
+     * @brief Write a log message to the output stream.
+     *
+     * @param msg The log message to write.
+     *
+     * If the log level of the message is lower than the log level of this output
+     * or if the log level is set to LogLevel::OFF, the message is not written.
+     *
+     * This function first formats the log message using the formatMessage
+     * function and then writes the formatted message to the output stream.
+     *
+     * If formatting fails, a message is written to the standard error stream
+     * indicating the error.
+     *
+     * If writing to the output stream fails, a message is written to the standard
+     * error stream indicating the error and the original log message.
+     */
     void log(const LogMessage& msg) {
         if (msg.level < logLevel || msg.level == LogLevel::OFF) return;
 
@@ -87,18 +113,60 @@ public:
         }
     }
 
+
+    /**
+     * @brief Set the log level for this logger.
+     *
+     * This function sets the log level for this logger instance. Log messages with a
+     * level lower than the set log level will be ignored and not written to the
+     * output stream.
+     *
+     * @param level The new log level to set.
+     */
+    void setLogLevel(LogLevel level) noexcept;
+    
+    
+    /**
+     * @brief Get the current log level for this logger.
+     *
+     * This function returns the current log level set for this logger instance.
+     *
+     * @return The current log level.
+     */
+    LogLevel getLogLevel() const noexcept;
     void setLogLevel(LogLevel level) noexcept { logLevel = level; }
     LogLevel getLogLevel() const noexcept { return logLevel; }
+
+    /**
+     * @brief Set the timestamp format string.
+     *
+     * This function sets the format string used to format the timestamp in log
+     * messages. The format string is used with std::strftime. The default format
+     * string is "%Y-%m-%d %H:%M:%S".
+     *
+     * @param format The format string.
+     */
     void setTimestampFormat(const std::string& format) {
         std::lock_guard<std::mutex> lock(streamMutex);
         timestampFormat = format;
     }
+    /**
+     * @brief Set the output format string.
+     *
+     * This function sets the format string used to format the output log messages.
+     * The format string is used with std::ostream. The default format string is
+     * "plain". The format string can be set to "json" to output log messages in
+     * JSON format.
+     *
+     * @param format The format string.
+     */
     void setOutputFormat(const std::string& format) {
         std::lock_guard<std::mutex> lock(streamMutex);
         outputFormat = format;
     }
+    
+    
     void enableANSIColors(bool enable) noexcept { useANSIColors = enable; }
-
     std::shared_ptr<std::ostream> getStream() const noexcept { return stream; }
 
 
@@ -111,6 +179,18 @@ private:
     mutable std::mutex streamMutex;
     mutable std::mutex errorMutex;
 
+    /**
+     * @brief Format a log message based on the output format.
+     *
+     * This function formats the given log message according to the output format
+     * set for this logger. If the output format is "json", the log message is
+     * formatted as a JSON object. Otherwise, the log message is formatted as a
+     * plain text string. The log message is returned as a string.
+     *
+     * @param msg The log message to format.
+     *
+     * @return The formatted log message as a string.
+     */
     std::string formatMessage(const LogMessage& msg) {
         std::ostringstream oss;
         std::string colorCode = useANSIColors ? getANSIColorCode(msg.level) : "";
@@ -152,6 +232,20 @@ private:
         return oss.str();
     }
 
+    /**
+     * @brief Returns an ANSI escape sequence to set the text color based on the log level.
+     *
+     * @param level The log level.
+     * @return The ANSI escape sequence for the given log level.
+     *
+     * The mapping of log levels to colors is as follows:
+     * - LogLevel::DEBUG: Cyan
+     * - LogLevel::INFO: Green
+     * - LogLevel::WARNING: Yellow
+     * - LogLevel::ERROR: Red
+     * - LogLevel::CRITICAL: Red background
+     * - All other values: No color (empty string)
+     */
     std::string getANSIColorCode(LogLevel level) const noexcept {
         switch (level) {
             case LogLevel::DEBUG: return "\033[36m"; // Cyan
@@ -163,6 +257,21 @@ private:
         }
     }
 
+    /**
+     * @brief Converts a LogLevel to its string representation.
+     *
+     * @param level The LogLevel to convert.
+     * @return The string representation of the LogLevel.
+     *
+     * The mapping of LogLevel values to strings is as follows:
+     * - LogLevel::DEBUG: "DEBUG"
+     * - LogLevel::INFO: "INFO"
+     * - LogLevel::WARNING: "WARNING"
+     * - LogLevel::ERROR: "ERROR"
+     * - LogLevel::CRITICAL: "CRITICAL"
+     * - LogLevel::OFF: "OFF"
+     * - All other values: "UNKNOWN"
+     */
     std::string logLevelToString(LogLevel level) const noexcept {
         switch (level) {
             case LogLevel::DEBUG: return "DEBUG";
@@ -205,7 +314,22 @@ public:
     Logger(const Logger&) = delete;
     Logger& operator=(const Logger&) = delete;
 
-    // Configuration methods
+    /**
+     * @brief Add a log output.
+     *
+     * @param os The output stream to which log messages are written.
+     * @param level The log level for this output.
+     * @param timestampFormat The timestamp format string.
+     * @param outputFormat The log message output format.
+     * @param useANSIColors Whether to use ANSI escape sequences to color log messages.
+     *
+     * This function adds a log output to the logger. The log output is configured
+     * with the given log level, timestamp format, output format, and whether to
+     * use ANSI escape sequences to color log messages. The log output is added to
+     * the list of outputs and the logger will write log messages to this output
+     * stream if the log level is greater than or equal to the log level of the
+     * output.
+     */
     void addOutput(std::shared_ptr<std::ostream> os, LogLevel level,
                    const std::string& timestampFormat = "%Y-%m-%d %H:%M:%S",
                    const std::string& outputFormat = "plain", bool useANSIColors = true) {
@@ -214,35 +338,94 @@ public:
         outputs.emplace_back(std::move(output));
     }
 
+    /**
+     * @brief Set the global log level for the logger.
+     *
+     * This function sets the global log level, which determines the minimum
+     * severity of log messages that will be processed by the logger. Log messages
+     * with a severity lower than the global log level will be ignored.
+     *
+     * @param level The LogLevel to set as the global log level.
+     */
     void setGlobalLogLevel(LogLevel level) noexcept {
         globalLogLevel.store(level, std::memory_order_relaxed);
     }
 
+    /**
+     * @brief Set the log level for a category.
+     *
+     * This function sets the log level for a category. Log messages with a
+     * severity lower than the log level set for a category will be ignored.
+     *
+     * @param category The category name.
+     * @param level The LogLevel to set for the category.
+     */
     void setCategoryLogLevel(const std::string& category, LogLevel level) {
         std::lock_guard<std::mutex> lock(categoriesMutex);
         categoryLogLevels[category] = level;
     }
 
+    /**
+     * @brief Add a category to the logger.
+     *
+     * This function adds a category to the logger. Log messages with a category
+     * that is not in the list of categories will be ignored.
+     *
+     * @param category The category name.
+     */
     void addCategory(const std::string& category) {
         std::lock_guard<std::mutex> lock(categoriesMutex);
         categories.insert(category);
     }
 
+    /**
+     * @brief Remove a category from the logger.
+     *
+     * This function removes a category from the logger. Log messages with a
+     * category that is not in the list of categories will be ignored.
+     *
+     * @param category The category name to remove.
+     */
     void removeCategory(const std::string& category) {
         std::lock_guard<std::mutex> lock(categoriesMutex);
         categories.erase(category);
     }
 
+    /**
+     * @brief Add a tag to the logger.
+     *
+     * This function adds a tag to the logger. Log messages with a tag that is
+     * not in the list of tags will be ignored.
+     *
+     * @param tag The tag name to add.
+     */
     void addTag(const std::string& tag) {
         std::lock_guard<std::mutex> lock(tagsMutex);
         tags.insert(tag);
     }
 
+    /**
+     * @brief Remove a tag from the logger.
+     *
+     * This function removes a tag from the logger. Log messages with a tag that
+     * is not in the list of tags will be ignored.
+     *
+     * @param tag The tag name to remove.
+     */
     void removeTag(const std::string& tag) {
         std::lock_guard<std::mutex> lock(tagsMutex);
         tags.erase(tag);
     }
 
+    /**
+     * @brief Set the timestamp format for all outputs.
+     *
+     * This function sets the format string used to format the timestamp in log
+     * messages for all output destinations managed by the logger. The format
+     * string is used with std::strftime. The default format string is "%Y-%m-%d %H:%M:%S".
+     *
+     * @param format The format string.
+     */
     void setTimestampFormat(const std::string& format) {
         std::lock_guard<std::mutex> lock(outputsMutex);
         for (const auto& output : outputs) {
@@ -250,6 +433,15 @@ public:
         }
     }
 
+    /**
+     * @brief Set the output format for all outputs.
+     *
+     * This function sets the format string used to format the log messages
+     * for all output destinations managed by the logger. The format string
+     * can be "plain" for plain text or "json" for JSON formatted log messages.
+     *
+     * @param format The format string to set for the output log messages.
+     */
     void setOutputFormat(const std::string& format) {
         std::lock_guard<std::mutex> lock(outputsMutex);
         for (const auto& output : outputs) {
@@ -257,6 +449,18 @@ public:
         }
     }
 
+    /**
+     * @brief Enable or disable asynchronous logging.
+     *
+     * This function controls the asynchronous logging mechanism. When enabled,
+     * log messages are processed in a separate thread, allowing the main thread
+     * to continue execution without waiting for logging to complete. When
+     * disabled, the logging thread is stopped, and any remaining messages in
+     * the queue are processed.
+     *
+     * @param enable If true, asynchronous logging is enabled. If false,
+     * asynchronous logging is disabled.
+     */
     void enableAsynchronousLogging(bool enable) {
         bool expected = !enable;
         if (asynchronousLogging.compare_exchange_strong(expected, enable)) {
@@ -276,6 +480,14 @@ public:
         }
     }
 
+    /**
+     * @brief Flush the log queue and output streams.
+     *
+     * This function will wait until all log messages in the queue have been processed
+     * by the logging thread (if asynchronous logging is enabled) and then flush all
+     * output streams. This is useful for ensuring that all log messages are written to
+     * their output destinations before the program exits.
+     */
     void flush() {
         if (asynchronousLogging.load(std::memory_order_acquire)) {
             std::unique_lock<std::mutex> lock(queueMutex);
@@ -290,6 +502,17 @@ public:
         }
     }
 
+    /**
+     * @brief Load the logger configuration from a JSON file.
+     *
+     * @param configFilePath The path to the JSON configuration file.
+     *
+     * This function will load the logger configuration from the given JSON
+     * file and apply it to the logger. The configuration file should contain
+     * the same format as the configuration accepted by the Logger::initializeFromConfig
+     * function. If the configuration file cannot be loaded or parsed, an
+     * exception is thrown.
+     */
     void loadConfiguration(const std::string& configFilePath) {
         initializeFromConfig(configFilePath);
     }
@@ -415,6 +638,16 @@ private:
         }
     }
 
+    /**
+     * @brief Function for processing log messages in a separate thread.
+     *
+     * This function runs in a separate thread when asynchronous logging is enabled.
+     * It continuously checks for new log messages in the queue and processes them
+     * by writing them to the configured outputs. The thread waits for new messages
+     * to be added to the queue or until the asynchronous logging is disabled. 
+     * Once disabled, it ensures all remaining messages in the queue are processed 
+     * before the thread exits.
+     */
     void loggingThreadFunction() {
         while (isRunning.load(std::memory_order_acquire)) {
             std::unique_lock<std::mutex> lock(queueMutex);
@@ -439,6 +672,16 @@ private:
         }
     }
 
+    /**
+     * @brief Get the current timestamp as a formatted string.
+     *
+     * This function retrieves the current system time and formats it
+     * into a string representation using the format "%Y-%m-%d %H:%M:%S".
+     * It is compatible with both MSC and non-MSC environments for
+     * converting the time to a local `std::tm` structure.
+     *
+     * @return The current timestamp formatted as a string.
+     */
     std::string getCurrentTimestamp() const {
         auto now = std::chrono::system_clock::now();
         std::time_t time_t_now = std::chrono::system_clock::to_time_t(now);
@@ -455,6 +698,34 @@ private:
         return ss.str();
     }
 
+    /**
+     * @brief Load the logger configuration from a JSON file.
+     *
+     * This function will load the logger configuration from the given JSON
+     * file and apply it to the logger. The configuration file should contain
+     * the same format as the configuration accepted by the Logger::initializeFromConfig
+     * function. If the configuration file cannot be loaded or parsed, an
+     * exception is thrown.
+     *
+     * The format of the JSON configuration file should be as follows:
+     * @code{.json}
+     * {
+     *     "globalLogLevel": "DEBUG",
+     *     "outputs": [
+     *         {
+     *             "type": "file",
+     *             "filePath": "server.log",
+     *             "logLevel": "DEBUG",
+     *             "timestampFormat": "%Y-%m-%d %H:%M:%S",
+     *             "outputFormat": "plain",
+     *             "useANSIColors": false
+     *         }
+     *     ]
+     * }
+     * @endcode
+     *
+     * @param configFilePath The path to the JSON configuration file.
+     */
     void initializeFromConfig(const std::string& configFilePath) {
         std::ifstream configFile(configFilePath);
         if (!configFile.is_open()) {
@@ -498,6 +769,17 @@ private:
         }
     }
 
+    /**
+     * @brief Convert a log level string to a LogLevel.
+     *
+     * This function takes a string representation of a log level and converts
+     * it to a LogLevel enumeration value. The possible string values are
+     * "DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL", and "OFF". If an
+     * invalid log level string is given, the function returns LogLevel::INFO.
+     *
+     * @param levelStr The log level string to convert.
+     * @return The LogLevel enumeration value for the given log level string.
+     */
     LogLevel stringToLogLevel(const std::string& levelStr) const noexcept {
         if (levelStr == "DEBUG") return LogLevel::DEBUG;
         if (levelStr == "INFO") return LogLevel::INFO;
